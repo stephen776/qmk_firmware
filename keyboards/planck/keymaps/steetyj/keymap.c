972a037cf4ef2ac5a27b4fccdfd12fd916643497
Stephen Jacobs <stephenjacobs@gmail.com>
2020-01-31 15:58:33 -0500

    add backslash back

--- keyboards/planck/keymaps/steetyj/keymap.c
+++ keyboards/planck/keymaps/steetyj/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,  KC_9,    KC_0,    KC_BSPC,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,  KC_6,    KC_MINS, _______,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,  KC_6,    KC_MINS, KC_BSLS,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,  KC_3,    KC_PLUS, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_0,  _______, _______, _______
 ),
