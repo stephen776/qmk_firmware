64432f8817eb1d1eda8b3ac6f623153f6418063a
Stephen Jacobs <sjacobs@truefit.io>
2020-02-06 12:16:55 -0500

    upate steetyj symbols layer on lower

--- keyboards/planck/keymaps/steetyj/keymap.c
+++ keyboards/planck/keymaps/steetyj/keymap.c
@@ -98,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, _______, _______, _______, _______, _______, _______, KC_EQL, KC_UNDS,  KC_LBRC, KC_RBRC, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
